fada1ac35de4d8223f79f86ae79e782c8ff392f7
rdeleon-dev <rdeleon.dev@gmail.com>
2026-01-05 22:02:26 -0500

    added count characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -8,5 +8,8 @@ def main():
     contents = get_book_text('books/frankenstein.txt')
     num_words = count_words(contents)
     print(f"Found {num_words} total words")
+
+    characters = count_characters(contents)
+    print(characters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,14 @@
 def count_words(text):
     return len(text.split())
+
+def count_characters(text):
+    characters = {}
+    
+    for c in text:
+        c = c.lower()
+        if c in characters:
+            characters[c] += 1
+        else:
+            characters[c] = 1
+
+    return characters
