4a5c30058dad5b830bcaefdc181c69a087434566
rdeleon-dev <rdeleon.dev@gmail.com>
2026-01-05 21:50:40 -0500

    refactor main.py, moved count_words to stats.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import count_words
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-
-def count_words(text):
-    return len(text.split())
 
 def main():
     contents = get_book_text('books/frankenstein.txt')
