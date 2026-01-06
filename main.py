6fbdd544311e1384335515b342170a4e7ddce7ec
rdeleon-dev <rdeleon.dev@gmail.com>
2026-01-06 00:17:24 -0500

    added argument for filepath of books

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_words, count_characters, sort_items
 
 def get_book_text(file_path):
@@ -5,10 +7,15 @@ def get_book_text(file_path):
         return f.read()
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    book_path = sys.argv[1]
+    print(f"Analyzing book found at {book_path}..")
 
-    contents = get_book_text('books/frankenstein.txt')
+    contents = get_book_text(book_path)
     num_words = count_words(contents)
 
     print("----------- Word Count ----------")
